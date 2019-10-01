b3ced9375669e93083b6946f321bb3dbe293c275
McCoy <8932344+mjamison3@users.noreply.github.com>
2019-09-30 21:42:30 -0400

    objects are cool

---
+++ objects-in-array.js
@@ -0,0 +1,15 @@
+let cars = [
+  { year: 2005,
+    make: "BMW",
+    model: "X5"},
+
+  { year: 2019,
+    make: "Honda",
+    model: "CRV"},
+
+  { year: 2015,
+    make: "Nissan",
+    model: "Maxima"},
+];
+
+//console.log(cars);
