bf7db1a0fddc08700a95e0041ee8b06a7cb123e5
McCoy <8932344+mjamison3@users.noreply.github.com>
2019-09-30 21:09:23 -0400

    origin push to the github repository

---
+++ data-types.js
@@ -0,0 +1,21 @@
+let x = 'string';
+let num = 5;
+let bool = true;
+let nope = undefined;
+let nothing;
+let arrayNew = ['We', 'Code', 'For', 'Fun'];
+let objNew = [
+  {name: "Tone", 
+  age: 23,
+  sex: "male"},
+
+  {name: "Bee", 
+  age: 26,
+  sex: "female"},
+
+  {name: "Todd", 
+  age: 90,
+  sex: "male"}
+];
+
+let today = new Date();
